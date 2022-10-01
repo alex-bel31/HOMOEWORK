03dccd6702f5c4a08cdf0461c5b024c14be86dbd
alexb <v4004462@gmail.com>
2022-10-01 15:09:26 +0300

    task_4

---
+++ repo/task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число 1");
+string number1 = Console.ReadLine();
+int a = int.Parse(number1);
+Console.WriteLine("Введите число 2");
+string number2 = Console.ReadLine();
+int b = int.Parse(number2);
+
+
+ if (a > b)
+ {
+    Console.WriteLine(a);
+ }             
+else 
+{
+    Console.WriteLine(b);
+}
+                
