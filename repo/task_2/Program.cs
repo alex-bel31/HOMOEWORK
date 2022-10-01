395a8a1b3c6b7c5fbbd1839dc50cb04bfe682972
alexb <v4004462@gmail.com>
2022-10-01 16:25:19 +0300

    task_2

---
+++ repo/task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число 1");
+string number1 = Console.ReadLine();
+int a = int.Parse(number1);
+Console.WriteLine("Введите число 2");
+string number2 = Console.ReadLine();
+int b = int.Parse(number2);
+Console.WriteLine("Введите число 3");
+string number3 = Console.ReadLine();
+int c = int.Parse(number3);
+int maximum = a;
+
+
+ if (maximum < b)
+ {
+    maximum = b;
+ }             
+else 
+{
+    maximum = a;
+}
+    if(maximum < c)
+    {
+        maximum = c;
+    }
+    else
+    {
+        Console.Write(maximum);
+    }
+
