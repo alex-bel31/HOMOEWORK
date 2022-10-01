e6179b92263e3984625ea18836211a9cfc6edf4a
alexb <v4004462@gmail.com>
2022-10-01 14:26:09 +0300

    task_6

---
+++ repo/task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string number1 = Console.ReadLine();
+int x = int.Parse(number1);
+
+if(x % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    
+    Console.WriteLine("НЕТ");
+}
