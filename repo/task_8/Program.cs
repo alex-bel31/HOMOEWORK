d950affaf7e66e1d567735ba671439695a7c7f4e
alexb <v4004462@gmail.com>
2022-10-01 15:49:28 +0300

    task_8

---
+++ repo/task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+string number1 = Console.ReadLine();
+int n = int.Parse(number1);
+
+
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+    {
+       Console.Write(i); 
+       Console.Write(",");
+       
+    }
+        
+     
+
+        
+
+    
+
